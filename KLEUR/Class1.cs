d26d43a6810da15c708291cb0ceab43bdb49dff4
TWGuardian <twhiteguardian@gmail.com>
2020-08-22 18:44:46 +0200

    Initial C# project generation

---
+++ KLEUR/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLEUR
+{
+    public class Class1
+    {
+    }
+}
